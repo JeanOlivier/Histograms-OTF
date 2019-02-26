4e9d959c1f3ea0e25a1347ffeddf51d2277fd379
JeanOlivier <jean.olivier.simoneau@usherbrooke.ca>
2019-02-26 15:25:41 -0500

    Trying to handle paths a little better

--- histograms.py
+++ histograms.py
@@ -2,17 +2,27 @@
 # -*- coding: utf-8 -*-
 
 import ctypes
-import os
+import sys, os
 import platform
 from numpy.ctypeslib import ndpointer
 from numpy import zeros, fromstring
+from numpy import int8, uint8, int16, uint16
+from ctypes import c_uint8, c_int8, c_uint16, c_int16, c_double, c_int, c_uint64
+
+libpath = os.path.abspath(os.path.dirname(__file__))
+if not libpath in os.environ['PATH']:
+    os.environ['PATH'] = libpath+os.environ['PATH']
 
 plat_info = dict(plat=platform.system())
 if plat_info['plat'] == 'Windows':
-    plat_info['lib'] = './histograms.dll'
+    plat_info['lib'] = os.path.join(libpath, 'histograms.dll')
     plat_info['com'] = 'make histograms.dll'
+    # Adding cygwin libs path for windows
+    libspath = r'C:\cygwin64\usr\x86_64-w64-mingw32\sys-root\mingw\bin\;'
+    if libpath not in os.environ['PATH']:
+        os.environ['PATH'] = libspath+os.environ['PATH']  
 else:
-    plat_info['lib'] = './histograms.so'
+    plat_info['lib'] = os.path.join(libpath, 'histograms.so')
     plat_info['com'] = 'make histograms.so'
 
 
@@ -20,6 +30,21 @@ if not os.path.isfile(plat_info['lib']):
     raise IOError("{lib} is missing. To compile on {plat}:\n{com}\n".format(**plat_info))
 
 lib = ctypes.cdll[plat_info['lib']]
+
+# OpenMP stuff
+if plat_info["plat"] == "Windows":
+    omp = ctypes.CDLL('libgomp-1')
+else:
+    try:
+        omp = ctypes.CDLL("/usr/lib/gcc/x86_64-linux-gnu/7/libgomp.so")
+    except:
+        omp = ctypes.CDLL("libgomp.so")
+set_num_threads = omp.omp_set_num_threads
+set_num_threads.argtypes=(ctypes.c_int,)
+set_num_threads.restype=None
+get_num_threads = omp.omp_get_max_threads
+get_num_threads.restype=ctypes.c_int
+get_num_threads.argtypes=None
 
 
 def hist1dNbits(x, n=8):
